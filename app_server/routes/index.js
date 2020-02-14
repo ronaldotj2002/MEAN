9d489e3d45defb7c263f62daaec4aa2a4af423e2
ronaldotj2002 <ronaldotj2002@yahoo.com.br>
2020-02-13 22:23:45 -0300

    Deploy Heroku

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,9 @@
- var express = require('express');
+var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
-/* GET home page. */
-var homepageController = function(req, res){
-  res.render('index', {title: 'Express'});
-};
 
-router.get('/', homepageController);
+
+router.get('/', ctrlMain.index);
 
 module.exports = router;
